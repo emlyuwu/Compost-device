d602cf27a65afe01feb121d752ecd0eb17f4f75e
Emily <157151877+emlyuwu@users.noreply.github.com>
2024-04-25 17:49:08 +0000

    beginning main programming

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+import packages
+import utime as time
+# Get packages for display and other shit
+clock = 0
 def recieve():
     # Get temperature and moisture measurements
     t = 0
@@ -48,22 +51,71 @@ def advice(temp, moist):
     
     
 def refresh(temp, moist, code):
+    # format for lcd
+    print(f"Temperature is {temp}\nMoisture is {moist}.\n")
     if code == 0:
         #good
-    if code == 1:
+        print("Your compost heap has good temperature and moisture.")
+    elif code == 1:
         #dry
-    if code == 2:
+        print("Your compost is dry. Add some water.")
+    elif code == 2:
         #wet
-    if code == 3:
+        print("Your compost is too wet. Try turning it and/or adding dry material.")
+    elif code == 3:
         #hot
-    if code == 4:
+        print("Your compost is too hot. Turn it over soon.")
+    elif code == 4:
         #cold
-    if code == 5:
+        print("Your compost is too cold. Don't turn it until it stabilizes.")
+    elif code == 5:
         # cold and dry
-    if code == 6:
+        print("Your compost is cold and dry. Add some water and more organics.")
+    elif code == 6:
         # cold and wet
-    if code == 7:
+        print("Your compost is cold and wet. Try adding more dry material and let it sit.")
+    elif code == 7:
         # hot and dry
-    if code == 8:
+        print("Your compost is hot and dry. Add water and turn it.")
+    elif code == 8:
         # hot and wet
+        print("Your compost is hot and wet. Turn it more, and maybe add dry material.")
+
+def getbutton():
+    print("blah blah check which buttons pressed")
+
+def write(moist, temp, clock):
+    print("stuff")
     
+
+
+# Loop for while buttons are unpressed. top left is current, top right is graph
+# bottom left is for idle mode
+
+
+# I LOVE MULTICORE MICROCONTROLLERS!!!!!!!! I WILL RUN THE FRONTEND DISPLAY STUFF ON ONE CORE AND SAVE INFO ON ANOTHER
+
+
+def current():
+    while getbutton() == 1:
+        #show the current info
+        temp, moist = recieve()
+        code = advice(temp, moist)
+        refresh(temp, moist, code)
+        time.sleep(60)
+def graph():
+    while getbutton() == 2:
+        # show the graph
+        # make the graph
+        print("graph")
+        time.sleep(60)
+def idle():
+    while getbutton() == 3:
+        temp, moist = recieve()
+
+def bgsave():
+    while True:
+        time.sleep(3600)
+        clock =+ 1
+        temp, moist = recieve()
+        write(moist, temp, clock)
